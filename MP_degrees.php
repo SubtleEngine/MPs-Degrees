8c6b2f2d8241207c4b2e04f0ead4815080cf5e02
Subtle Engine <code@subtleengine.org>
2014-06-20 06:33:29 +0100

    Counts subjecta area by political party, categorises uncaught degrees as 'other' etc.

--- MP_degrees.php
+++ MP_degrees.php
@@ -6,27 +6,45 @@ ini_set('display_errors', 1);
 $mp_list = fopen("http://www.theyworkforyou.com/mps/?f=csv", 'r');
 $wiki_base_url = "http://en.wikipedia.org/w/api.php?action=parse&prop=text&format=json&page=";
 
-$subject_areas = array ( //Based on HESA subject area groups: http://www.hesa.ac.uk/dox/dataTables/studentsAndQualifiers/download/Subject1213.xlsx
-    "Medicine & dentistry" => 0,
-    "Subjects allied to medicine" => 0,
-    "Biological sciences" => 0,
-    "Veterinary sciences" => 0,
-    "Agriculture & related subjects" => 0,
-    "Physical sciences" => 0,
-    "Mathematical sciences" => 0,
-    "Computer sciences" => 0,
-    "Engineering & technology" => 0,
-    "Architecture, building & planning" => 0,
-    "Social studies" => 0,
-    "Law" => 0,
-    "Business & administrative studies" => 0,
-    "Mass communications & documentation" => 0,
-    "Languages" => 0,
-    "Historical & philosophical studies" => 0,
-    "Creative arts & design" => 0,
-    "Education" => 0
+$tallies = array ( //Based on parties in They Work for You's .csv
+    "Alliance" => 0,
+    "Conservative" => 0,
+    "DUP" => 0,
+    "Green" => 0,
+    "Independent" => 0,
+    "Labour" => 0,
+    "Liberal Democrat" => 0,
+    "Other" => 0,
+    "Plaid Cymru" => 0,
+    "Respect" => 0,
+    "Scottish National Party" => 0,
+    "Sinn Fein" => 0,
+    "Social Democratic and Labour Party" => 0
 );
 
+$subject_areas = array ( //Based on HESA subject area groups: http://www.hesa.ac.uk/dox/dataTables/studentsAndQualifiers/download/Subject1213.xlsx
+    "Medicine & dentistry" => $tallies,
+    "Subjects allied to medicine" => $tallies,
+    "Biological sciences" => $tallies,
+    "Veterinary sciences" => $tallies,
+    "Agriculture & related subjects" => $tallies,
+    "Physical sciences" => $tallies,
+    "Mathematical sciences" => $tallies,
+    "Computer sciences" => $tallies,
+    "Engineering & technology" => $tallies,
+    "Architecture, building & planning" => $tallies,
+    "Social studies" => $tallies,
+    "Law" => $tallies,
+    "Business & administrative studies" => $tallies,
+    "Mass communications & documentation" => $tallies,
+    "Languages" => $tallies,
+    "Historical & philosophical studies" => $tallies,
+    "Creative arts & design" => $tallies,
+    "Education" => $tallies,
+    "Other" => $tallies
+);
+
+//Known MPs whose Wiki page has an unusual suffix; script currently ignores
 //Willie_Bain
 //Geraint_Davies_(Labour_politician)
 //Geoffrey_Clifton-Brown_(born_1953)
@@ -42,6 +60,7 @@ while (($mp = fgetcsv($mp_list, 1000, ",")) !== FALSE) {
         $last_name = str_replace(" ", "_", $mp[2]); //Replace any spaces with underscores e.g. for Nick_de_Bois
         
         $subject_area = "";
+        unset($degree_matches);
         
         $wiki = json_decode(file_get_contents($wiki_base_url.$first_name."_".$last_name)); //Fetch the Wiki article
         $content = strip_tags($wiki->{'parse'}->{'text'}->{'*'}); //Retrieve text and strip HTML tags
@@ -75,100 +94,109 @@ while (($mp = fgetcsv($mp_list, 1000, ",")) !== FALSE) {
         
         //Categorise degrees by subject area
         
-        if (isset($degree_matches[2])) {
+        //print_r($degree_matches);
+        
+        if (isset($degree_matches[2]) && !empty($degree_matches[2])) {
         
             if (preg_match("/medicine|medical|doctor/im", implode(", ",$degree_matches[2]))) {
-                ++$subject_areas["Medicine & dentistry"];
+                ++$subject_areas["Medicine & dentistry"][$mp[3]];
                 $subject_area = "Medicine & dentistry";
             }
 
             elseif (preg_match("/medicine|physiolog|dentist/im", implode(", ",$degree_matches[2]))) {
-                ++$subject_areas["Subjects allied to medicine"];
+                ++$subject_areas["Subjects allied to medicine"][$mp[3]];
                 $subject_area = "Subjects allied to medicine";
             }
             
             elseif (preg_match("/biological/im", implode(", ",$degree_matches[2]))) {
-                ++$subject_areas["Biological sciences"];
+                ++$subject_areas["Biological sciences"][$mp[3]];
                 $subject_area = "Biological sciences";
             }
 
-            /*elseif (preg_match("//im", implode(", ",$degree_matches[2]))) {
-                ++$subject_areas["Veterinary sciences"];
+            /*elseif (preg_match("//im", implode(", ",$degree_matches[2]))) { //To do
+                ++$subject_areas["Veterinary sciences"][$mp[3]];
                 $subject_area = "Veterinary sciences";
             }*/
 
-            elseif (preg_match("/agricultur|land/im", implode(", ",$degree_matches[2]))) {
-                ++$subject_areas["Agriculture & related subjects"];
+            elseif (preg_match("/agricultur|land man/im", implode(", ",$degree_matches[2]))) {
+                ++$subject_areas["Agriculture & related subjects"][$mp[3]];
                 $subject_area = "Agriculture & related subjects";
             }
             
-            elseif (preg_match("/natural sciences|chemist|physic/im", implode(", ",$degree_matches[2]))) {
-                ++$subject_areas["Physical sciences"];
+            elseif (preg_match("/natural sciences|chemist|physic|bacteriolog/im", implode(", ",$degree_matches[2]))) {
+                ++$subject_areas["Physical sciences"][$mp[3]];
                 $subject_area = "Physical sciences";
             }
 
             elseif (preg_match("/mathematics|math/im", implode(", ",$degree_matches[2]))) {
-                ++$subject_areas["Mathematical sciences"];
+                ++$subject_areas["Mathematical sciences"][$mp[3]];
                 $subject_area = "Mathematical sciences";
             }
 
             elseif (preg_match("/computer/im", implode(", ",$degree_matches[2]))) {
-                ++$subject_areas["Computer sciences"];
+                ++$subject_areas["Computer sciences"][$mp[3]];
                 $subject_area = "Computer sciences";
             }
 
-            elseif (preg_match("/engineering/im", implode(", ",$degree_matches[2]))) {
-                ++$subject_areas["Engineering & technology"];
+            elseif (preg_match("/engineering|metallurg/im", implode(", ",$degree_matches[2]))) {
+                ++$subject_areas["Engineering & technology"][$mp[3]];
                 $subject_area = "Engineering & technology";
             }
 
             elseif (preg_match("/architect/im", implode(", ",$degree_matches[2]))) {
-                ++$subject_areas["Architecture, building & planning"];
+                ++$subject_areas["Architecture, building & planning"][$mp[3]];
+                $subject_area = "Architecture, building & planning";
             }
 
             elseif (preg_match("/economics|ppe|politics|political|social|sociology|geograph|government|human|psych/im", implode(", ",$degree_matches[2]))) {
-                ++$subject_areas["Social studies"];
+                ++$subject_areas["Social studies"][$mp[3]];
                 $subject_area = "Social studies";
             }
             
             elseif (preg_match("/law|llb|llm|solicitor|legal|justice|policy|jurisprudence/im", implode(", ",$degree_matches[2]))) {
-                ++$subject_areas["Law"];
+                ++$subject_areas["Law"][$mp[3]];
                 $subject_area = "Law";
             }
 
             elseif (preg_match("/business|accountan|management/im", implode(", ",$degree_matches[2]))) {
-                ++$subject_areas["Business & administrative studies"];
+                ++$subject_areas["Business & administrative studies"][$mp[3]];
                 $subject_area = "Business & administrative studies";
             }
 
             elseif (preg_match("/communication|journalism/im", implode(", ",$degree_matches[2]))) {
-                ++$subject_areas["Mass communications & documentation"];
+                ++$subject_areas["Mass communications & documentation"][$mp[3]];
                 $subject_area = "Mass communications & documentation";
             }
 
             elseif (preg_match("/languages|german|french|english|russia|serbo|literature|litt/im", implode(", ",$degree_matches[2]))) {
-                ++$subject_areas["Languages"];
+                ++$subject_areas["Languages"][$mp[3]];
                 $subject_area = "Languages";
             }
             
-            elseif (preg_match("/histor|philosophy|classic/im", implode(", ",$degree_matches[2]))) {
-                ++$subject_areas["Historical & philosophical studies"];
+            elseif (preg_match("/histor|philosophy|theolog|classic/im", implode(", ",$degree_matches[2]))) {
+                ++$subject_areas["Historical & philosophical studies"][$mp[3]];
                 $subject_area = "Historical & philosophical studies";
             }
             
             elseif (preg_match("/music/im", implode(", ",$degree_matches[2]))) {
-                ++$subject_areas["Creative arts & design"];
+                ++$subject_areas["Creative arts & design"][$mp[3]];
                 $subject_area = "Creative arts & design";
             }
             
             elseif (preg_match("/education/im", implode(", ",$degree_matches[2]))) {
-                ++$subject_areas["Education"];
+                ++$subject_areas["Education"][$mp[3]];
                 $subject_area = "Education";
+            }
+            
+            else {
+                ++$subject_areas["Other"][$mp[3]];
+                $subject_area = "Other";
+                $other[$mp[1]." ".$mp[2]] = $degree_matches[2];
             }
             
         }
 
-        echo "$mp[1]\t$mp[2]"; //Output name
+        echo "$mp[1]\t$mp[2]\t$mp[3]"; //Output name
 
         if (isset($uni_matches[2])) {
             echo "\t".$uni_matches[2]; //Output university if found
@@ -202,8 +230,28 @@ while (($mp = fgetcsv($mp_list, 1000, ",")) !== FALSE) {
 
 echo "\n\n";
 
+//Output the results by party
+
+echo "Subject Area\t";
+
+foreach ($tallies as $key => $value) {
+    echo "$key\t"; //Header row of parties
+}
+
+echo "\n";
+
 foreach ($subject_areas as $key => $value) {
-    echo "$key\t$value\n";
+    echo "$key\t"; //Subject area header
+    foreach ($value as $k => $v) {
+        echo "$v\t"; //Each party's tally
+    }
+    echo "\n";
+}
+
+echo "\n\n";
+
+foreach ($other as $key => $value) {
+    //echo "$key\t".implode(";",$value)."\n"; //For checking uncategorised degrees
 }
 
 ?>
--- README.md
+++ README.md
@@ -7,10 +7,14 @@ This PHP script:
 - Searches the article’s text for mention of their university, degree and any other occupation
 - Categorises those degrees into one of eighteen groups
 
-See the results of the script here: http://subtleengine.org/post/85447902730/mps-degrees-what-do-they-know
+See the results of the script here: http://subtleengine.org/2014/05/11/mps-degrees-what-do-they-know/
 
 This is not elegant or particularly reliable code. Pull requests welcome!
 
-Run the script like this:
+Run the script like this to save the output to a file:
 
 ``php ./MP_degrees.php > MP_Uni_output.csv``
+
+or use tee to see output while saving to a file:
+
+``php ./MP_degrees.php | tee MP_Uni_output.csv``
